02f42719534cee953eaf24f62e52b5af90b8e1c2
Vinicius Vieira <v.vieira.batista@outlook.com>
2022-03-24 07:45:55 -0300

    Add orm and orm config and create contact entitie

---
+++ src/api/contacts/entities/contact.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Contact {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  full_name: string;
+
+  @Column()
+  phone?: string;
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @UpdateDateColumn()
+  updatedAt?: Date;
+
+  @DeleteDateColumn()
+  deletedAt?: Date;
+}
