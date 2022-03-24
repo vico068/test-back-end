194373a76126b787c82e84137f61f33e769189a5
Vinicius Vieira <v.vieira.batista@outlook.com>
2022-03-24 10:31:19 -0300

    Add lib swagger and add schemas in dto (create docs)

--- src/api/contacts/dto/create-contact.dto.ts
+++ src/api/contacts/dto/create-contact.dto.ts
@@ -1,12 +1,15 @@
 import { Contact } from './../entities/contact.entity';
 import { IsNotEmpty, IsPhoneNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateContactDto extends Contact {
+  @ApiProperty()
   @IsNotEmpty({
     message: 'Informe um nome',
   })
   full_name: string;
 
+  @ApiProperty()
   @IsPhoneNumber('BR', {
     message: 'Informe um numero de telefone valido',
   })
--- src/api/contacts/dto/update-contact.dto.ts
+++ src/api/contacts/dto/update-contact.dto.ts
@@ -1,12 +1,15 @@
 import { Contact } from './../entities/contact.entity';
 import { IsNotEmpty, IsPhoneNumber, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateContactDto extends Contact {
+  @ApiProperty()
   @IsNotEmpty({
     message: 'Informe um nome',
   })
   full_name: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsPhoneNumber('BR', {
     message: 'Informe um numero de telefone valido',
